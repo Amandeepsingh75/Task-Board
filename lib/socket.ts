1facfcf52a0cfa04c92d38b86b760b855d9ab180
Amandeepsingh75 <amandeep75318@gmail.com>
2025-05-19 15:07:19 +0530

    add socket backend url

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3002");
+const socket = io("https://task-board-socket-backend-1.onrender.com");
 
 export default socket;
